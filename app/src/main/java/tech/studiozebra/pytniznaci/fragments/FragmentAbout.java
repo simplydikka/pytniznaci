1b8df2df9d60560b93a35e4f18c07d05cbf51a8d
simplydikka <diddeto@gmail.com>
2018-12-05 01:01:07 +0200

    Test ads implemented Splash screen migrated Categories grid redesigned All categories added All rules added for a test SDK issues fixed Package name changed successfully

---
+++ app/src/main/java/tech/studiozebra/pytniznaci/fragments/FragmentAbout.java
@@ -0,0 +1,22 @@
+package tech.studiozebra.pytniznaci.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tech.studiozebra.pytniznaci.R;
+
+public class FragmentAbout extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about, container, false);
+
+    }
+
+
+}
